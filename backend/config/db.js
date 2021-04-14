7a658797048e08dc69dd6c91d4de738e8bdfed8d
Babatunde Onasanya <onasanyatunde67@gmail.com>
2021-04-14 22:50:42 +0100

    Mongo db connect successful

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  try {
+    mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log("Mongodb connect success");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 app.use(express.json());
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
