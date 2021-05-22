21ca66b4e638e016fa24a94542aa725edbc61175
Babatunde Onasanya <onasanyatunde67@gmail.com>
2021-05-22 21:49:28 +0100

    Setup login structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,33 @@
 import "./App.css";
-import { Link } from "@chakra-ui/react";
+import {
+  Flex,
+  Button,
+  Spacer,
+  Box,
+  Heading,
+  Container,
+} from "@chakra-ui/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import BaseRouter from "./routes";
 
 function App() {
   return (
     <Router>
-      <BaseRouter />
-      <Link href="/login">Login</Link>
-      <Link href="/register">Register</Link>
+      <Flex>
+        <Box p="4">
+          <Heading size="md">Chakra App</Heading>
+        </Box>
+        <Spacer />
+        <Box p="4">
+          <Button colorScheme="teal" mr="4">
+            Sign Up
+          </Button>
+          <Button colorScheme="teal">Log in</Button>
+        </Box>
+      </Flex>
+      <Container maxW="container.xl">
+        <BaseRouter />
+      </Container>
     </Router>
   );
 }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,28 @@
+import { Grid, Box, Image, Center, Flex, Text } from "@chakra-ui/react";
 import React from "react";
+import LoginImage from "../assets/images/login.svg";
 
 const Login = () => {
-  return <div>Welcome to login</div>;
+  return (
+    <>
+      {/* <Grid templateColumns="repeat(2, 1fr)" gap={3} justifyContent="center">
+        <Box boxSize="md"></Box>
+        <Box>
+          <p></p>
+        </Box>
+      </Grid> */}
+
+      <Flex>
+        <Center flex='0.5'>
+          <Image src={LoginImage} alt="Login" />
+        </Center>
+
+        <Box flex="0.5">
+          <Text>Welcome to login</Text>
+        </Box>
+      </Flex>
+    </>
+  );
 };
 
 export default Login;
