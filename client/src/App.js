b1721fc203db9d040ab8b0eb1f02bb4540691aec
Babatunde Onasanya <onasanyatunde67@gmail.com>
2021-06-09 20:46:16 +0100

    Added readme

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ import {
   IconButton,
 } from "@chakra-ui/react";
 import { BrowserRouter as Router } from "react-router-dom";
-import BaseRouter, { MissingPageRouter } from "./routes";
+import BaseRouter from "./routes";
 import CartDrawer from "./components/CartDrawer";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useSelector, useDispatch } from "react-redux";
